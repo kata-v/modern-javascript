f737fa3c2d90b55feda75f2875298077489f6635
KataV <KataV@users.noreply.github.com>
2020-02-06 17:06:59 +0200

    Lessons from 10 to 14

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,13 +1,13 @@
 // numbers
 
-let radius = 10;
-let pi = 3.14;
+//let radius = 10;
+//let pi = 3.14;
 
 // console.log(radius, pi);
 
 // math operators - +, -, *, /, **, %
 
-// console.log(10 / 2);
+ //console.log(10 / 2);
 // let result = radius % 3;
 // let result = pi * radius**2;
 
@@ -18,7 +18,7 @@ let pi = 3.14;
 // console.log(result);
 
 // shorthands
-let likes = 10;
+//let likes = 10;
 
 // likes = likes + 1;
 // likes++;
@@ -36,13 +36,73 @@ let likes = 10;
 // console.log(5 / 'hello');
 // console.log(5 * 'hello');
 
-let result = 'the blog has ' + likes + ' likes.';
-console.log(result);
+//let result = 'the blog has ' + likes + ' likes.';
+//console.log(result);
+
+//-----Lesson 12----
+
+//template strings or template literals
+//const title = 'Best reads of 2019';
+//const author = 'Mario';
+//const likes = 30;
+
+
+//concetation way
+//let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes.';
+//console.log(result)
+
+//template string way
+//let result = `The blog called ${title} by ${author} has ${likes} likes`;
+//console.log(result);
+
+
+//creating html templates
+//let html = `<h2> ${title}</h2>
+//<p>By ${author}</p>
+//<span>This blog has ${likes} likes </span>
+//`;
+
+//Tää ainoastaan näyttää sen consolissa tägeineen kaikkineen. 
+//Miten käytetään sisällön tuotantoon näytetään myöhemmin.
+//console.log(html);
 
 
 
 
 
+//-----Lesson 13-----
+
+//let ninjas = ['shaun', 'ryu', 'chun-li'];
+
+//ninjas[1] = 'ken';
+
+//console.log(ninjas[1])
+
+//let ages = [20,25,30,35];
+//console.log(ages[2]);
+
+//let random = ['shaun', 'crystal', 30, 20];
+//console.log(random);
+
+//console.log(ninjas.length);
 
 
+//array methods
+//let result = ninjas.join('-');
+//let result = ninjas.indexOf('chun-li');
+//let result = ninjas.concat(['ken', 'crystal']);
+//let result = ninjas.push('ken');
+//result = ninjas.pop();
 
+//console.log(result);
+
+
+//----Lesson 14----- Null ja undefined. Undefined tulee automaattisesti.
+// Null pitää itse määrittää
+
+let age = null;
+
+console.log(age, age + 3, `the age is ${age}`)
+
+
+//----Lesson 15-----
