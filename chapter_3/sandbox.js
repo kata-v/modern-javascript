49d4311d91e38de959be15e2d83d3fb645bf3c67
KataV <KataV@users.noreply.github.com>
2020-03-05 20:32:29 +0200

    Update sandbox.js

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -4,10 +4,29 @@
 //   console.log('loop: ', i);
 // }
 
+//const names = ['shaun', 'mario', 'luigi'];
+
+//for(let i = 0; i < names.length; i++){
+  // console.log(names[i]);
+  //let html = `<div>${names[i]}</div>`;
+  //console.log(html);
+//}
+
+//-----chapter 20----
+//while loops
+
+//let i = 0;
+
+//while(1 < 5) { 
+ // console.log('in loop: ', i);
+ // i++;
+//}
+
 const names = ['shaun', 'mario', 'luigi'];
 
-for(let i = 0; i < names.length; i++){
-  // console.log(names[i]);
-  let html = `<div>${names[i]}</div>`;
-  console.log(html);
+let i = 0;
+
+while(i< names.length){
+  console.log(names[i]);
+  i++;
 }
